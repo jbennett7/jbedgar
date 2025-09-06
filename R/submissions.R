db332e294b5a4fb06a536be70fbaf64da5634dfb
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2025-09-06 08:36:24 -0600

    fix reference to getCik

--- R/submissions.R
+++ R/submissions.R
@@ -1,5 +1,5 @@
 submissions <- function(ticker){
-    cik <- get_cik(ticker)
+    cik <- getCik(ticker)
     if(is.na(cik))
         stop(paste0('Ticker: "', ticker, '" does not exist.'))
     slink <- paste0("https://data.sec.gov/submissions/CIK", cik, ".json")
